1982159b40dfd49fa5b637797f1593165a2625db
TonyTrNhPh <trnhphquang190703@gmail.com>
2026-07-30 13:56:40 +0700

    feat: add more Dogs

--- Assets/Scripts/SO/BossData.cs
+++ Assets/Scripts/SO/BossData.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "SO/BossData")]
 public class BossData : ScriptableObject
 {
-    public string bossName;
-    public string bossDescription;
-    
-    public GameObject skin;
+    [Header("Dog Description")]
+    public string dogName;
+    public string dogDescription;
+
+    [Header("Dog Stat")]
     public float damage = 10;
+    public float reloadTime = 1;
+    public float health = 100;
+    public float distance = 10;
     
-    //---------- Animation Speed ----------//
-    public float attackSpeed = 1;
-    public float shootVFXSpeed = 1;
-    public float projectileSpeed = 1;
+    [Header("Dog Skin")]
+    public GameObject skin;
 }
--- Assets/Scripts/SO/DogData.cs
+++ Assets/Scripts/SO/DogData.cs
@@ -3,18 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "SO/DogData")]
 public class DogData : ScriptableObject
 {
+    [Header("Dog Description")]
     public string dogName;
     public string dogDescription;
-    public DogLevelData[] dogLevels;
-}
 
-[System.Serializable]
-public class DogLevelData
-{
-    public GameObject skin;
+    [Header("Dog Stat")]
     public float damage = 10;
     public float reloadTime = 1;
-    
     public float health = 100;
     public float distance = 10;
+    
+    [Header("Dog Skin")]
+    public GameObject skin;
 }
